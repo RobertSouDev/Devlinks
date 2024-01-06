ac21db734d9983919af39ddc42e4452b1029e8c2
RobertSouDev <rogerhallks1@gmail.com>
2024-01-05 23:48:24 -0300

    esse commit voltei em um codigo antigo e vi como eu tinha feito o caminho para pega a foto e mudei ele para um teste

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function toggleMode(){
     // substituir a imagem
     if(html.classList.contains('light')) {
     // se tiver light mode, adicionar a imagem light
-        img.setAttribute('src', './assets/avatar-light.png')
+        img.setAttribute('src', 'https://media.istockphoto.com/id/532378051/pt/foto/c%C3%A9u-de-noite-com-estrelas-e-nuvens.jpg?s=1024x1024&w=is&k=20&c=7uPrU3gBCjpoYlSgRAaPGYG-lts3AZL7ZNWbS-8Nrmw=')
     } else{
     // se tiver sem light mode, manter a imagem normal
         img.setAttribute('src', './assets/avatar.png')
